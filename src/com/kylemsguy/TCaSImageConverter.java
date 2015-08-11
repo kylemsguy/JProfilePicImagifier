d9c286d84817d8c1d3fbf059202450b4f8e9ef62
Kyle Zhou <kylezhou2002@hotmail.com>
2015-08-10 20:48:55 -0700

    Added Alpha blending with White as bgcolour

--- src/com/kylemsguy/TCaSImageConverter.java
+++ src/com/kylemsguy/TCaSImageConverter.java
@@ -15,6 +15,8 @@ import java.io.IOException;
  *  - TODO: figure them all out
  */
 public class TCaSImageConverter {
+    private static final int[] BACKGROUND_COLOUR = {255, 255, 255}; // This is the proper spelling. Trust me. I'm Canadian.
+
     private int[] argbAry;
 
     private void commonInit(File file) throws IOException {
@@ -42,34 +44,50 @@ public class TCaSImageConverter {
     public String convertToTImg(){
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < argbAry.length - 1; i++){
-            //int alpha = argbAry[i] >> 24 & 255;
-            int red = argbAry[i] >> 16 & 255;
-            int green = argbAry[i] >> 8 & 255;
-            int blue = argbAry[i] & 255;
+            int[] rgb = ARGB2RGB(argbAry[i], BACKGROUND_COLOUR);
 
-            sb.append(red / 4);
+            sb.append(rgb[0] / 4);
             sb.append("|");
-            sb.append(green / 4);
+            sb.append(rgb[1] / 4);
             sb.append("|");
-            sb.append(blue / 4);
+            sb.append(rgb[2] / 4);
             sb.append(",");
 
         }
 
-        int lastPixel = argbAry[argbAry.length - 1];
+        int[] lastPixel = ARGB2RGB(argbAry[argbAry.length - 1], BACKGROUND_COLOUR);
 
-        //int alpha = lastPixel >> 24 & 255;
-        int red = lastPixel >> 16 & 255;
-        int green = lastPixel >> 8 & 255;
-        int blue = lastPixel & 255;
-
-        sb.append(red / 4);
+        sb.append(lastPixel[0] / 4);
         sb.append("|");
-        sb.append(green / 4);
+        sb.append(lastPixel[1] / 4);
         sb.append("|");
-        sb.append(blue / 4);
+        sb.append(lastPixel[2] / 4);;
 
         return sb.toString();
+    }
+
+    /**
+     * Applies a background colour to an int packed ARGB value
+     * @param argb Integer packed ARGB value
+     * @param background RGB array for the background colour (length 3)
+     * @return An array of RGB values (length 3)
+     */
+    public static int[] ARGB2RGB(int argb, int[] background){
+        int alpha = argb >> 24 & 255;
+
+        int red = argb >> 16 & 255;
+        int green = argb >> 8 & 255;
+        int blue = argb & 255;
+
+        int newRed = pixelARGB2RGB(alpha, red, background[0]);
+        int newGreen = pixelARGB2RGB(alpha, green, background[1]);
+        int newBlue = pixelARGB2RGB(alpha, blue, background[2]);
+        return new int[]{newRed, newGreen, newBlue};
+    }
+
+    private static int pixelARGB2RGB(int alpha, int original, int background){
+        int opacity = alpha / 255;
+        return opacity * original + (1-opacity) * background;
     }
 
     public String toString() {
